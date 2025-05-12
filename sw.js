210d0553a647978cd71e957a400b4aad3903e8b5
muhammadafiqyunanto27 <muhammadafiqyunanto@gmail.com>
2025-05-12 11:29:09 +0700

    update version

--- sw.js
+++ sw.js
@@ -1,39 +1,38 @@
-// Tambahkan versi cache
-const CACHE_NAME = 'kalkulator-v3'; // ubah versinya jika update
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/idb.js',
-  '/manifest.json',
-  '/icon.png'
+const CACHE_NAME = "kalkulator-cache-v1";
+const assets = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/idb.js",
+  "/manifest.json",
+  "/icon.png"
 ];
 
-// Install Service Worker
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
   );
+  self.skipWaiting();
 });
 
-// Activate - Hapus cache lama
-self.addEventListener('activate', event => {
+self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
       )
     )
   );
+  self.clients.claim();
 });
 
-// Fetch
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
+    })
   );
 });
